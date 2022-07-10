781293d33be2e176795090cf8742af9bb684780e
Tino Reyna <tinoreyna1984@gmail.com>
2022-07-09 21:37:26 -0500

    Se corrige problema de build de Vite

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,9 +1,13 @@
 
 export const getEnvVariables = () => {
-    import.meta.env;
+    /* import.meta.env;
 
     return {
         ...import.meta.env,
-    };
+    }; */
+
+    return {
+        VITE_API_URL: import.meta.env.VITE_API_URL,
+    }
 
 }
